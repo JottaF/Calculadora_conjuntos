cab6b915afd5a24f2fb36a7cc69c72cff286a43a
João Felipe <69224917+JottaF@users.noreply.github.com>
2021-08-12 17:28:46 -0300

    Adição de novas funções

--- Conjuntos/conjunto.py
+++ Conjuntos/conjunto.py
@@ -1,30 +1,108 @@
 class Conjunto:
-    def __init__(self, nome):
+    def __init__(self):
         self.conjunto = []
+        self.nome = 'Sem nome'
+    
+    def nomear(self, nome):
         self.nome = nome
     
     def inserir(self, elemento):
               self.conjunto.append(str(elemento))
     
     def imprimir(self):
-        print('{', (', ').join(sorted(self.conjunto)), '}')
+        print(self.nome,'= {', (', ').join(sorted(self.conjunto)), '}')
 
     def pertence(self, elemento):
         return elemento in self.conjunto
 
     def contido(self, elemento):
-        spot = True
         for i in self.conjunto:
             if i not in elemento.conjunto:
-                spot = False
-        return spot
+                return False
 
+        return True
     
     def uniao(self, elemento):
-        uniao = []
-        uniao.copy()
+        uniao = Conjunto()
+        for i in self.conjunto:
+            uniao.inserir(i)
+
         for i in elemento.conjunto:
-            if not i in uniao:
-                uniao.append(i)
+            if not i in uniao.conjunto:
+                uniao.inserir(i)
         
-        return sorted(uniao)
+        return uniao
+    
+    def igual(self, elemento):
+        if len(self.conjunto) == len(elemento.conjunto):
+            for i in self.conjunto:
+                if i not in elemento.conjunto:
+                    return False
+        else:
+            return False
+        return True
+
+    def interseccao(self, elemento):
+        result = Conjunto()
+        for i in self.conjunto:
+            if i in elemento.conjunto:
+                result.inserir(i)
+        
+        return result
+    
+    def diferenca(self, elemento):
+        result = Conjunto()
+        for i in self.conjunto:
+            if i not in elemento.conjunto:
+                result.inserir(i)
+
+        return result
+    
+    # def __addCDP__(self, size, conjunto):
+
+        # print('entrou na função\n')
+        # if len(conjunto) and size <= len(conjunto):
+        #     print('entrou no if\n')
+        #     result = Conjunto()
+        #     size = size
+        #     index = 0
+
+        #     print('size: ', size,'\nindex: ',index)
+
+        #     a = []
+
+        #     for i in range(len(conjunto)):
+        #         print('posição: ',conjunto[i])
+        #         if size == 0:
+        #             result.inserir(i)
+        #             a.append(i)
+        #         else:
+        #             print('entrou no else\n')
+        #             sub = Conjunto()
+        #             b = []
+        #             if (index + size) <= len(conjunto):
+        #                 for j in range(size):
+        #                     print(index,size, len(conjunto))
+        #                     sub.inserir(conjunto[index+j])
+        #                     b.append(index+j)
+                    
+        #             sub.imprimir()
+        #             result.inserir(sub)
+        #             a.append(b)
+        #             print(a)
+        #         index += 1
+
+        #     return result.inserir(self.__addCDP__(size+1, conjunto))
+
+    
+    def conjuntoDasPartes(self):
+        conjunto = []
+        conjunto.append([])
+
+        for i in self.conjunto:
+            conjunto.append([i])
+        
+        
+
+        return conjunto
+
--- Conjuntos/main.py
+++ Conjuntos/main.py
@@ -1,12 +1,16 @@
 from conjunto import Conjunto
 
-a = Conjunto('A')
-b = Conjunto('B')
-c = Conjunto('C')
+a = Conjunto()
+a.nomear('A')
+b = Conjunto()
+b.nomear('B')
+c = Conjunto()
+c.nomear('C')
 
 a.inserir(2)
 a.inserir(1)
 a.inserir(5)
+a.inserir(6)
 
 a.imprimir()
 
@@ -15,14 +19,34 @@ b.inserir(4)
 b.inserir(6)
 
 
+c.inserir(6)
 c.inserir(2)
 c.inserir(4)
-c.inserir(6)
 
+print('União')
 print(a.uniao(b))
 
 a.imprimir()
 
+print('\nContido')
 print(b.contido(a))
 print(b.contido(c))
 print(c.contido(b))
+
+print('\nIgual')
+print(c.igual(b))
+print(a.igual(c))
+print(b.igual(c))
+
+print('\nConjunto D')
+d = a.interseccao(c)
+d.nomear('D')
+d.inserir(5)
+d.imprimir()
+
+print('\nDiferença')
+e = a.diferenca(b)
+e.nomear('E')
+e.imprimir()
+
+print(a.conjuntoDasPartes())
